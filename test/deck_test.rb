d4c16f3aa1b7708e1f51fbc40e24e5ed5b0ddc12
distalphalanges <ioan.a.mercea@gmail.com>
2020-07-25 15:34:22 +1000

    Create tests for Deck

---
+++ test/deck_test.rb
@@ -0,0 +1,42 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+  def test_it_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_it_has_cards
+    # skip
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal cards, deck.cards
+  end
+
+  def test_it_can_count_cards
+    # skip
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
+  end
+
+end
