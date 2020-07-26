510864ca36e2aaf8dd5d24117fd14b8a1334b08d
distalphalanges <ioan.a.mercea@gmail.com>
2020-07-26 16:41:05 +1000

    Implement Deck sorting

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,13 @@ class Deck
   def count
     cards.count
   end
+
+  def sort
+    sorted = []
+    cards.each do |unsorted_card|
+      place = sorted.rindex { |sorted_card| sorted_card < unsorted_card } || -1
+      sorted.insert(place + 1, unsorted_card)
+    end
+    sorted
+  end
 end
